00182c0294cec9f2da230fcf656e277f9ecd68dc
JuanDaf <juandaforer@gmail.com>
2021-12-03 19:16:44 -0500

    configuracion db

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask.wrappers import Request
 from flask_mysqldb import MySQL
 
 app =  Flask(__name__)
@@ -16,8 +18,37 @@ def index():
 
 @app.route('/pacientes')
 def pacientes():
-
     return render_template('pacientes.html')
+
+@app.route('/addpacientes',methods=['POST'])
+def addpacientes():
+    if request.method == 'POST':
+        documento = request.form['documento']
+        nombres = request.form['nombres']
+        apellidos = request.form['apellidos']
+        edad = request.form['edad']
+        direccion = request.form['direccion']
+        sexo = request.form.get('sexo')
+        fumador = request.form.get('fumador1')
+        fumadorr = request.form['fumador']
+        estatura = request.form['estatura']
+        peso = request.form['peso']
+        dieta = request.form.get('dieta')
+
+        if fumador == "1":
+            fumador = fumadorr +' años' 
+            con = mysql.connection.cursor()
+            con.execute('INSERT INTO pacientes VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+            (documento,nombres,apellidos,edad,direccion,sexo,peso,estatura,fumador,dieta,12,'ok','ok',4))
+            mysql.connection.commit()
+        else :
+            fumador = 'no'
+            con = mysql.connection.cursor()
+            con.execute('INSERT INTO pacientes VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+            (documento,nombres,apellidos,edad,direccion,sexo,peso,estatura,fumador,dieta,12,'ok','ok',4))
+            mysql.connection.commit()
+        print(documento, nombres, apellidos, edad, direccion, sexo, peso, estatura, fumador, dieta)
+    return 'bien'
 
 @app.route('/verpacientes')
 def ver():
